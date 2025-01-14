676d6850aa95833cf1ea0e658bf7aa519e50b9be
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-01-14 15:12:04 -0800

    Add editor space default to script

--- addons/screwloose-editor-settings/screwloose-editor-settings.gd
+++ addons/screwloose-editor-settings/screwloose-editor-settings.gd
@@ -5,3 +5,4 @@ func _enter_tree():
   var settings = EditorInterface.get_editor_settings()
   # Use spaces to see more code on GitHub and standardize indent width.
   settings.set_setting("text_editor/behavior/indent/type", 1)
+  settings.set_setting("text_editor/behavior/indent/size", 4)
