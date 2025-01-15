5dcd6350116d9ec1fff1e5ddea6537fc22f7959d
ChristophGray <chrisgraydev@gmail.com>
2025-01-15 19:13:14 +0000

    Bug fixes
    -Fixed indentation on my code
    -MusicController is now enabled in global variables
    -Uncommented start button OnStopTitle
    -Fixed pause menu not checking for a previous track
    -if pause menu doesn't detect a previous track it just stops playing

--- globals/music/music_controller.gd
+++ globals/music/music_controller.gd
@@ -15,48 +15,52 @@ var track_before_pause: AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  GlobalSignalBus.title_screen_started.connect(_on_start_title)
-  GlobalSignalBus.level_started.connect(_on_start_level)
-  GlobalSignalBus.altar_activated.connect(_on_altar_begin)
-  GlobalSignalBus.game_paused.connect(_on_start_pause)
-  GlobalSignalBus.game_unpaused.connect(_on_stop_pause)
+    GlobalSignalBus.title_screen_started.connect(_on_start_title)
+    GlobalSignalBus.level_started.connect(_on_start_level)
+    GlobalSignalBus.altar_activated.connect(_on_altar_begin)
+    GlobalSignalBus.game_paused.connect(_on_start_pause)
+    GlobalSignalBus.game_unpaused.connect(_on_stop_pause)
   
-  if get_tree().current_scene.name == "main_menu":
-    _on_start_title()
+    if get_tree().current_scene.name == "main_menu":
+        _on_start_title()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-  pass
+    pass
 
 func set_music_state(new_state: MUSIC_STATE) -> void:
-  previous_state = current_state
-  current_state = new_state
+    previous_state = current_state
+    current_state = new_state
 
 func _on_start_title() -> void:
-  set_music_state(MUSIC_STATE.Title)
-  SoundManager.music.play(title_music, 0.0, 1.0, 2.0, "")
+    set_music_state(MUSIC_STATE.Title)
+    SoundManager.music.play(title_music, 0.0, 1.0, 2.0, "")
   
 func _on_stop_title() -> void:
-  SoundManager.music.stop(2.0)
+    SoundManager.music.stop(2.0)
   
 func _on_start_pause() -> void:
-  previous_state = current_state
-  set_music_state(MUSIC_STATE.Pause)
-  #elapsed_music_time = track_before_pause.get_playback_position()
-  SoundManager.music.play(pause_music, 0.0, 1.0, 0.25, "")
+    previous_state = current_state
+    set_music_state(MUSIC_STATE.Pause)
+    if track_before_pause:
+        elapsed_music_time = track_before_pause.get_playback_position()
+    SoundManager.music.play(pause_music, 0.0, 1.0, 0.25, "")
   
 ## Resume from previous song time
 func _on_stop_pause() -> void:
-  set_music_state(previous_state)
-  var previous_stream = track_before_pause.stream
-  track_before_pause = SoundManager.music.play(previous_stream, elapsed_music_time, 1.0, 0.25, "")
+    set_music_state(previous_state)
+    if track_before_pause:
+        var previous_stream = track_before_pause.stream
+        track_before_pause = SoundManager.music.play(previous_stream, elapsed_music_time, 1.0, 0.25, "")
+    else:
+        SoundManager.music.stop(0.25)
   
 func _on_start_level() -> void:
-  if (current_state != MUSIC_STATE.Level):
-    set_music_state(MUSIC_STATE.Level)
-    track_before_pause = SoundManager.music.play(level_music, 0.0, 1.0, 1.5, "")
+    if (current_state != MUSIC_STATE.Level):
+        set_music_state(MUSIC_STATE.Level)
+        track_before_pause = SoundManager.music.play(level_music, 0.0, 1.0, 1.5, "")
     
 func _on_altar_begin() -> void:
-  if (current_state != MUSIC_STATE.Altar):
-    set_music_state(MUSIC_STATE.Altar)
-    track_before_pause = SoundManager.music.play(battle_music, 0.0, 1.0, 0.5, "")
+    if (current_state != MUSIC_STATE.Altar):
+        set_music_state(MUSIC_STATE.Altar)
+        track_before_pause = SoundManager.music.play(battle_music, 0.0, 1.0, 0.5, "")
