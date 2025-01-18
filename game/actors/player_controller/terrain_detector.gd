e906ccc1deb5296ff25b613af0a7518b8c181127
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2025-01-17 17:09:40 -0800

    Add env

--- game/actors/player_controller/terrain_detector.gd
+++ game/actors/player_controller/terrain_detector.gd
@@ -7,8 +7,15 @@ func _ready() -> void:
     collision_layer = 0
     collision_mask = TerrainArea.TERRAIN_TYPE_COLLISION_LAYER
     area_entered.connect(_on_area_entered)
+    area_exited.connect(_on_area_exited)
 
 func _on_area_entered(area: Area3D):
     if area is TerrainArea:
         current_terrain_type = area.type
         print(area.TerrainType, current_terrain_type)
+
+func _on_area_exited(area: Area3D):
+    if area is TerrainArea:
+        var overlapping = get_overlapping_areas()
+        if overlapping.size() <= 0:
+            current_terrain_type = TerrainArea.TerrainType.NONE
